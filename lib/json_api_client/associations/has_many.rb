b3e4cbbb303e4dd1508652c8ceb46a693aae5766
Jeremy Lecerf <redpist@redpist.com>
2017-06-01 10:48:14 +0200

    automatic shortcut methods to relationships

--- lib/json_api_client/associations/has_many.rb
+++ lib/json_api_client/associations/has_many.rb
@@ -5,7 +5,34 @@ module JsonApiClient
 
       module ClassMethods
         def has_many(attr_name, options = {})
-          self.associations = self.associations + [HasMany::Association.new(attr_name, self, options)]
+            association = HasMany::Association.new(attr_name, self, options)
+           self.send :define_method, attr_name do
+            get_has_many_relationship(association)
+          end
+          self.associations = self.associations + [association]
+        end
+      end
+
+      def get_has_many_relationship(association)
+        unless instance_variable_get(:"@#{association.attr_name}").nil?
+          instance_variable_get(:"@#{association.attr_name}")
+        else
+          relateds = []
+          relateds_to_query = []
+          relationships.send(association.attr_name.to_sym)['data'].each do |related|
+            relationship_id = related['id']
+            relationship_type = related['type']
+            if defined?(last_result_set.included.data) && last_result_set.included.data[relationship_type]
+              included_relationship = last_result_set.included.data[relationship_type][relationship_id]
+            end
+            if included_relationship
+              relateds << included_relationship
+            else
+              relateds_to_query << relationship_id
+            end
+          end
+          relateds = association.association_class.where(id: relateds_to_query.uniq.join(',')).find if relateds_to_query.size > 0
+          instance_variable_set(:"@#{association.attr_name}", relateds)
         end
       end
 
--- lib/json_api_client/associations/has_one.rb
+++ lib/json_api_client/associations/has_one.rb
@@ -5,7 +5,30 @@ module JsonApiClient
 
       module ClassMethods
         def has_one(attr_name, options = {})
-          self.associations += [HasOne::Association.new(attr_name, self, options)]
+         association = HasOne::Association.new(attr_name, self, options)
+
+          self.send :define_method, attr_name do
+            get_has_one_relationship(association)
+          end
+
+          self.associations += [association]
+        end
+      end
+
+      def get_has_one_relationship(association)
+        relationship_name = association.attr_name
+        unless instance_variable_get(:"@#{relationship_name}").nil?
+          instance_variable_get(:"@#{relationship_name}")
+        else
+          relationship_data = relationships.send(relationship_name.to_sym)['data']
+          if defined?(last_result_set.included) && last_result_set.included.data_for(relationship_name, relationship_data)
+            included_relationship = last_result_set.included.data_for(relationship_name, relationship_data)
+          end
+          if included_relationship
+            instance_variable_set(:"@#{relationship_name}", included_relationship)
+          else
+            instance_variable_set(:"@#{relationship_name}", association.association_class.find(relationship_data['id']).first)
+          end
         end
       end
 
