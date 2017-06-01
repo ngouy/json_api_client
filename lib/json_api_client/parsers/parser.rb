6146b199843220fa412ffdc22c87195db1c26f81
Jeremy Lecerf <redpist@redpist.com>
2017-06-01 11:08:25 +0200

    rollback result_set storage

--- lib/json_api_client/parsers/parser.rb
+++ lib/json_api_client/parsers/parser.rb
@@ -9,13 +9,13 @@ module JsonApiClient
             result_set.record_class = klass
             result_set.uri = response.env[:url]
             handle_json_api(result_set, data)
+            handle_data(result_set, data)
             handle_errors(result_set, data)
             handle_meta(result_set, data)
             handle_links(result_set, data)
             handle_relationships(result_set, data)
             handle_pagination(result_set, data)
             handle_included(result_set, data)
-            handle_data(result_set, data)
           end
         end
 
@@ -48,10 +48,9 @@ module JsonApiClient
         #  }
         #
         #
-        def parameters_from_resource(params, result_set)
+        def parameters_from_resource(params)
           attrs = params.slice('id', 'links', 'meta', 'type', 'relationships')
           attrs.merge(params.fetch('attributes', {}))
-          attrs.merge!({result_set: result_set})
         end
         private
 
